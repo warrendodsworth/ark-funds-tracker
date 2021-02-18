59061ceea5104ac3761da10f53c1f2553087345d
Warren Dodsworth <warren.dodsworth@gmail.com>
2021-02-19 08:39:00 +1100

    add setup to config

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+# setup timescaledb in docker - set pwd on first run
+# docker run -d --name timescaledb -p 5432:5432 -e POSTGRES_PASSWORD=password timescale/timescaledb:latest-pg12 
+# docker exec -it timescaledb bash
+# psql -U postgres
+
+
+
 API_URL = 'https://paper-api.alpaca.markets'
 API_KEY = 'yourkey'
 API_SECRET = 'yoursecret'
